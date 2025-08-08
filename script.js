097c7e13d2e7b264ee648038a7357d1dc61622b2
John Gallardo <johnpatrickgallardo33@gmail.com>
2025-08-07 21:38:39 -0600

    Feat: added 3 global variables for every operation (operator, num1, & num2) and finished operate() function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let operator = null;
+let num1 = 0, num2 = 0;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +15,24 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    let result;
+    switch (operator) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = subtract(num1, num2);
+            break;
+        case "multiply":
+            result = multiply(num1, num2);
+            break;
+        case "divide":
+            result = divide(num1, num2);
+            break;
+        default:
+            alert("Error: Invalid operator");
+    }
 }
